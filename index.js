95bd065186eb396095adfb757ba29ad8f3c86744
Patrick Kuo <patrick5267@gmail.com>
2017-09-01 15:18:03 +0800

    replace nightmare.js with puppeteer

--- index.js
+++ index.js
@@ -1,100 +1,113 @@
-var Nightmare = require('nightmare');
-var nightmare = Nightmare({ show: true })
-var cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const cheerio = require('cheerio');
 
-const ENTRIES_MUL_MINUTE = 6000;
+let page;
 
-function shouldClick(entry) {
-  if(entry.time.indexOf('hour') !== -1) {
-    return
+const saveCookie = (cookies) => (new Promise((resolve) => {
+  fs.writeFile('cookies.json', JSON.stringify(cookies), 'utf8', resolve);
+}));
+
+const loadCookie = () => (new Promise((resolve) => {
+  fs.exists('cookies.json', (exists) => {
+    if (exists) {
+      fs.readFile('cookies.json', 'utf8', (err, data) => {
+        resolve(JSON.parse(data));
+      });
+    } else {
+      resolve([]);
+    }
+  });
+}));
+
+const shouldClick = (entry) => {
+  if(entry.time.indexOf('hours remain') !== -1) {
+    return false;
   }
-  if(entry.value < ENTRIES_MUL_MINUTE) {
+  if(entry.entries < 500) {
     return true;
   }
-
+  else {
+    return false
+  }
 }
 
-function parseEntry(el) {
-  var time = el.find('.giveaway__columns').text().trim();
-  var name = el.find('.giveaway__heading__name').text();
-  var link = el.find('.giveaway__heading__name').attr('href');
-  var minute = parseInt(time.split('minutes')[0]);
-  var entries = parseInt(el.find('.giveaway__links a span').first().text().split('entries')[0].replace(/,/g, ""));
+const parseEntry = (el) => {
+  const time = el.find('.giveaway__columns').text().trim();
+  const name = el.find('.giveaway__heading__name').text();
+  let link = el.find('.giveaway__heading__name').attr('href');
+  link = `https://www.steamgifts.com${link}`;
+  const minute = parseInt(time.split('minutes')[0]);
+  const entries = parseInt(el.find('.giveaway__links a span').first().text().split('entries')[0].replace(/,/g, ""));
+  let copy = 1;
 
-  var result = {
-    time: time,
-    name: name,
-    link: link,
-    minute: minute,
-    entries: entries,
-    value: minute*entries,
+  const heading = el.find('.giveaway__heading').text().trim();
+  if(heading.indexOf('Copies') !== -1) {
+    copy = parseInt(heading.split('Copies')[0].split('(')[1]);
+    if(isNaN(copy)) {
+      copy = 1;
+    }
+  }
+
+  const result = {
+    time,
+    name,
+    link,
+    minute,
+    entries: entries/copy,
   }
 
   return result;
 }
 
-function click(target, callback) {
-  nightmare
-    .goto('https://www.steamgifts.com/' + target.link)
-    .wait('body')
-    // .click('form[action*="/search"] [type=submit]')
-    .click('.sidebar__entry-insert')
-    .then(function() {
-      console.log(target.link + '... clicked!');
-      setTimeout(function(){
-        callback();
-      }, 4000);
-    })
-}
-
-function clickThrough(targets) {
-  if(targets.length == 0) {
-    console.log('No target remained');
-    return;
-  }
-  click(targets[0], function() {
-    targets.shift();
-    clickThrough(targets);
+const click = ({ link }) => (
+  new Promise(async (resolve) => {
+    try {
+      await page.goto(link);
+      await page.waitFor('body');
+      await page.click('.sidebar__entry-insert');
+    } catch (e) {
+      console.log(e);
+    }
+    await page.waitFor(4000);
+    resolve();
   })
-}
+);
 
-function start() {
-  nightmare
-    .goto('https://www.steamgifts.com/')
-    // .type('form[action*="/search"] [name=p]', 'github nightmare')
-    .wait('.page__outer-wrap')
-    .evaluate(function () {
-      return document.querySelector('.page__outer-wrap').innerHTML;
-    })
-    .then(function (result) {
-      let $ = cheerio.load(result);
-      var targets = [];
+const frontPage = async () => {
+  let cookies = await loadCookie();
+  await Promise.all(cookies.map((c) => page.setCookie(c)));
+  await page.goto('https://www.steamgifts.com/');
+  await page.waitFor('.nav__button-container');
+  console.log('Logined Front Page');
+  cookies = await page.cookies();
+  saveCookie(cookies);
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  const targets = [];
 
-      $('.giveaway__row-inner-wrap').each(function(i, el) {
-        if(!$(this).hasClass('is-faded')) {
-          
-          var obj = parseEntry($(this));
+  $('.giveaway__row-inner-wrap').each(function(i, el) {
+    if(!$(this).hasClass('is-faded')) {
+      const obj = parseEntry($(this));
+      if(shouldClick(obj)) {
+        targets.push({ link: obj.link });
+      }
+    }
+  })
 
-          if(shouldClick(obj)) {
-            targets.push({link:obj.link, value:obj.value});
-          }
+  while (targets.length > 0) {
+    await click(targets.shift());
+  }
 
-        }
-      })
+  await page.waitFor(180000);
+  frontPage();
+};
 
-      targets.sort(function(a, b) {
-        return a.value - b.value;
-      })
+const main = async () => {
+  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, headless: false });
+  page = await browser.newPage();
+  frontPage();
+};
 
-      clickThrough(targets);
-    })
-    .catch(function (error) {
-      console.error('Search failed:', error);
-    });
+main();
 
-    setTimeout(function() {
-      start();
-    }, 10*60*1000);
-}
-
-start();
